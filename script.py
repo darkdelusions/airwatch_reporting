920e1be06601fd155b9d9d2e4c9bd193693d0b45
Andy Brown <darkdelusions@gmail.com>
2021-04-12 13:02:56 -0500

    remove unneeded date test function

--- script.py
+++ script.py
@@ -32,6 +32,7 @@ def get_device_data():
     device_dataset.columns = ['_user_name','user_full_name','_user_email','_device_platform','device_model','_device_serial_number','_event_created_time_utc','device_imei','device_current_carrier','device_phone_number','device_enrollment_status',' device_enrollment_date_utc','device_asset_number']	
     return(device_dataset)
 
+#Gets the device data need and creates a pandas dataframe
 def get_user_data():
     endpoint_url = config['authentication']["airwatch_url"] + "/API/system/users/search"
     query_string = {"orderby":"user" , "pagesize":"15000"}
@@ -43,17 +44,12 @@ def get_user_data():
     users_dataset.columns = ['_user_email','user_department','user_custom_attribute_1','user_custom_attribute_5']
     return(users_dataset)
 
+#Merges the device \ user data
 def merge_data(df1,df2):
     date = datetime.now().strftime("%Y-%m-%d")
     merged_dataset = pd.merge(df1, df2, how='outer', on='_user_email')
     clean_merged_dataset = merged_dataset.replace({r'\s+$': '', r'^\s+': ''}, regex=True).replace(r'\n',  ' ', regex=True)
     clean_merged_dataset.to_csv(f'ITAM Report-' + date + '.csv',  index=False)
-
-
-def date_test():
-    date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
-    print(f'ITAM Report-' + str(date) + '.csv')
-
 
 def main():
     df1 = get_device_data()
